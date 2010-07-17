16d58b22153d94f516cdb0127ea55b2e4c5e9293
Donn Felker <dfelker@gmail.com>
2010-07-17 10:35:21 -0500

    Adding small item changes for chapters 1-6

--- src/src/com/dummies/android/silentmodetoggle/MainActivity.java
+++ src/src/com/dummies/android/silentmodetoggle/MainActivity.java
@@ -4,83 +4,81 @@ import android.app.Activity;
 import android.graphics.drawable.Drawable;
 import android.media.AudioManager;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity {
-   
+
+	private AudioManager mAudioManager;
 	private boolean mPhoneIsSilent;
-	 
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        setContentView(R.layout.main);
-        
-        
-        setButtonClickListener(); 
-    }
-    
-    @Override
-    protected void onResume() {
-    	super.onResume(); 
-    	checkIfPhoneIsSilent(); 
-    	toggleUi(); 
-    };
-    
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.main);
+
+		mAudioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+		setButtonClickListener();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		checkIfPhoneIsSilent();
+		toggleUi();
+	};
 
 	private void toggleUi() {
-		
-		ImageView imageView = (ImageView)findViewById(R.id.phone_icon);
-		Drawable newPhoneImage; 
-		
-		if(mPhoneIsSilent)
-		{
-			newPhoneImage = getResources().getDrawable(R.drawable.phone_silent);  
-			 
+
+		ImageView imageView = (ImageView) findViewById(R.id.phone_icon);
+		Drawable newPhoneImage;
+
+		if (mPhoneIsSilent) {
+			newPhoneImage = getResources().getDrawable(R.drawable.phone_silent);
+
 		} else {
 			newPhoneImage = getResources().getDrawable(R.drawable.phone_on);
 		}
-		
+
 		imageView.setImageDrawable(newPhoneImage);
 	}
 
 	private void setButtonClickListener() {
-		Button toggleButton = (Button)findViewById(R.id.toggleButton); 
+		Button toggleButton = (Button) findViewById(R.id.toggleButton);
 		toggleButton.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				AudioManager audioManager = (AudioManager)getSystemService(AUDIO_SERVICE); 
-				if(mPhoneIsSilent)
-				{
+
+				if (mPhoneIsSilent) {
 					// Change back to normal mode
-					audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL); 
-					mPhoneIsSilent = false; 
+					mAudioManager
+							.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+					mPhoneIsSilent = false;
 				} else {
 					// Change to silent mode
-					audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
-					mPhoneIsSilent = true; 
+					mAudioManager
+							.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+					mPhoneIsSilent = true;
 				}
-				
-				// Now toggle the UI again 
-				toggleUi(); 
+
+				// Now toggle the UI again
+				toggleUi();
 			}
-		}); 
-		
+		});
+
 	}
 
 	private void checkIfPhoneIsSilent() {
-		AudioManager audioManager = (AudioManager)getSystemService(AUDIO_SERVICE); 
-		int ringerMode = audioManager.getRingerMode(); 
-		if(ringerMode == AudioManager.RINGER_MODE_SILENT)
-		{
-			mPhoneIsSilent = true; 
+		int ringerMode = mAudioManager.getRingerMode();
+		if (ringerMode == AudioManager.RINGER_MODE_SILENT) {
+			mPhoneIsSilent = true;
 		} else {
-			mPhoneIsSilent = false; 
+			mPhoneIsSilent = false;
 		}
-			
+
 	}
 }
